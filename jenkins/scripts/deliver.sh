3247da3b031b858bcd565a380495a62f25512879
abdulmajidmomin <64473180+abdulmajidmomin@users.noreply.github.com>
2020-06-01 19:06:15 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo 'add new line to test build'
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
